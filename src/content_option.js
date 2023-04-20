261e71d41c41a8cb5243d2caebc9edaf69deabd2
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-20 21:38:12 +0100

    .

--- src/content_option.js
+++ src/content_option.js
@@ -1,166 +1,190 @@
-const logotext = "JOHN";
+const logotext = "Marziyeh";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "Marziyeh Azhdari",
+  description:
+    "I’m Marziyeh Azhdari _ Front-end developer,currently Studying in London",
 };
 
 const introdata = {
-    title: "I’m John Doe",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Marziyeh Azhdari",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop responsive websites",
+  },
+  description:
+    "I specialize in creating dynamic and responsive user interfaces as a front-end developer with a keen eye for design and a passion for joining functionality and aesthetics seamlessly.",
+  your_img_url:
+    "https://images.unsplash.com/photo-1491975474562-1f4e30bc9468?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGRldmVsb3BlciUyMGdpcmx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+  title: "abit about my self",
+  aboutme: `I am an accomplished chemistry graduate with three years of experience working
+as a process chemist at Kimia Pars in Tehran, Iran. During my time there, I
+worked with an excellent team to run and interpret analytical data for validation
+and quality control. Together, we successfully produced a product of exceptional
+quality that helped our company win the market. Since moving to the UK two
+years ago, I have discovered a passion for technology, particularly in the field of
+computer science. As a creative person, I find that software development allows
+me to combine my technical skills with my love of problem-solving. I am currently
+enrolled in a highly collaborative, intensive 9-month 20hr/week software
+development course with Code Your Future (CYF), where I am learning new
+skills and gaining experience in web development.`,
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+const worktimeline = [
+  {
+    jobtitle: "Software Development Course",
+    where: "CodeYourFuture ",
+    date: "2022-2023",
+  },
+  {
+    jobtitle: "learn English",
+    where: "London & Oxford",
+    date: "2021-2022 ",
+  },
+  {
+    jobtitle: "Process Chemist employee ",
+    where: " Kimia Pars",
+    date: "2017-2020",
+  },
+  {
+    jobtitle: "Bachelor's degree in Chemistry",
+    where: "Iran ",
+    date: "2013-2017",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "Javascript",
+    value: 60,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "HTML",
+    value: 85,
+  },
+  {
+    name: "CSS",
+    value: 65,
+  },
+  {
+    name: "Git, GitHub",
+    value: 70,
+  },
+  {
+    name: "Node.js,and Express",
+    value: 20,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+  {
+    title: "Ability to create responsive websites",
+    description:
+      "I take pleasure in leveraging my skillset to design and develop responsive websites with ease and proficiency.",
+  },
+  {
+    title: "problem-solving",
+    description:
+      "Excellent problem-solving skills with an eye for designing solutions that are resilient and scalable.",
+  },
+  {
+    title: "Ability to work as a team member",
+    description:
+      "I am a collaborative individual with a strong aptitude for teamwork, and I am capable of effectively contributing to group efforts towards achieving shared goals.",
+  },
+  {
+    title: "self-motivated",
+    description:
+      "I am a self-motivated individual who takes initiative and strives for excellence",
+  },
+  {
+    title: "learn new skills",
+    description:
+      "I possess a strong passion for learning and continuously seek out opportunities to acquire new skills and knowledge to enhance my personal and professional growth.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW1hem9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    description:
+      "I created this website with React on my own and one similar with a group of my classmates.",
+    link: "https://phenomenal-entremet-a2ff2a.netlify.app/",
+  },
+  {
+    img: "https://media.istockphoto.com/id/1471867479/photo/hotel-5-stars-gold-color-sign.jpg?b=1&s=170667a&w=0&k=20&c=0iz6pRC5oeHrzakVf0npQQvuqPNaM94TCM0EFkI9IdA=",
+    description: "This is my Hotel React App that I created through my course.",
+    link: "https://marziyeh-hotel-react-app.netlify.app/",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1586202690180-1967f682ef1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fFRvJTIwZG8lMjBsaXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    description: "The simple To Do List",
+    link: "https://marziyeh-todo-list.netlify.app/",
+  },
+  {
+    img: "https://media.istockphoto.com/id/119816727/photo/piggy-bank-and-dominos.jpg?s=612x612&w=0&k=20&c=T7oE_U3DAvmXSrfBI8jA6UUj8l1RBLKhjf6EqDmxbHA=",
+    description: "Pig game dice.",
+    link: "https://pig-game-by-mari-azhdari-enjoy.netlify.app/",
+  },
+  {
+    img: "https://media.istockphoto.com/id/89484374/photo/enigma.jpg?s=612x612&w=0&k=20&c=A7A0QHXxdOyFoHcI3ZJpzlfRSc1qShQ-s-W4F3xgMpw=",
+    description: "Guess My Number Game",
+    link: "https://inquisitive-bunny-fe1592.netlify.app/",
+  },
+  {
+    img: "https://media.istockphoto.com/id/1132715308/photo/rear-view-of-a-couple-watching-tv-while-relaxing-on-the-sofa-at-home.jpg?s=612x612&w=0&k=20&c=aS3pf153ReHseuaCjrZgsaJcrIO6zWUciWp7DE9ioao=",
+    description: "TV Project like  TV MAZE",
+    link: "https://cyf-mariazhdari-tv.netlify.app/",
+  },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+  {
+    img: "https://media.istockphoto.com/id/1415219089/photo/roulette-wheel-in-casino-with-a-ball.jpg?s=612x612&w=0&k=20&c=TcAd4iDKvOtlAh7Gg19zNyCnTgY2pnTgzWPsfprcSOo=",
+    description: "Number Facts",
+    link: "https://candid-bavarois-42dbe5.netlify.app/",
+  },
+  {
+    img: "https://media.istockphoto.com/id/835703212/photo/karma.jpg?s=612x612&w=0&k=20&c=BuZA3_WlyT-Za-Y2Hm9zkVyNFRIlVLvjoCzicUeKKGQ=",
+    description: "Karma project.",
+    link: "https://cheerful-paprenjak-c290c2.netlify.app/",
+  },
+  {
+    img: "https://media.istockphoto.com/id/1298626388/photo/e-donation-concept.jpg?s=612x612&w=0&k=20&c=ExACKu3PclaPYivL6UmcgklDmyPZeYoxS5jTkreashY=",
+    description: "just design",
+    link: "https://lucky-pavlova-65b5c3.netlify.app/",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "Mrazi.aghchay@gmail.com",
+  YOUR_FONE: "07503336272",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com/MariAzhdari",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/marziyeh-azhdari-3685b0246/",
+  twitter: "https://twitter.com/MariAzhdari",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
